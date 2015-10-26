e061abc3182e2c739c851fc23e6796018009709e
Freddy Rodriguez <cristinamar2000@gmail.com>
2015-10-26 12:33:48 -0430

    delete server folder

--- server/middlewares/index.js
+++
@@ -1,9 +0,0 @@
-var json_server_middlewares = require('./json-server-middleware');
-
-var middlewares = [json_server_middlewares];
-
-module.exports = function(server){
-  for (var i = 0; i < middlewares.length; i++){
-    middlewares[i](server);
-  }
-};
--- server/middlewares/json-server-middleware.js
+++
@@ -1,9 +0,0 @@
-module.exports = function(server){
-
-  var jsonServer = require('json-server');
-
-  server.use(jsonServer.defaults());
-
-  var router = jsonServer.router('db.json');
-  server.use(router);
-}
