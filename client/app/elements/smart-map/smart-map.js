ab1ef42f5d346fb3d72d8da6d13273f33b9516e3
Freddy Rodriguez <cristinamar2000@gmail.com>
2015-10-26 17:29:21 -0430

    confim and complaint mark

--- client/app/elements/smart-map/smart-map.js
+++ client/app/elements/smart-map/smart-map.js
@@ -13,29 +13,35 @@ Polymer({
   loadCurrentPos: function(){
     if (navigator.geolocation) {
         setTimeout(this.loadCurrentPos.bind(this), 5000);
-        return navigator.geolocation.getCurrentPosition(this.initMap.bind(this));
+        return navigator.geolocation.getCurrentPosition(
+          this.updateCurrentPosMarker.bind(this));
     } else {
         throw  'Geolocation is not supported.';
     }
   },
-  initMap: function(location) {
+  updateCurrentPosMarker: function(location){
     this.pos = {
       lat: location.coords.latitude,
       lng: location.coords.longitude
     };
 
-    this.map = document.querySelector('google-map');
-    this.map.latitude = this.pos.lat;
-    this.map.longitude = this.pos.lng;
-    this.map.zoom = 15;
+    if (!this.map){
+      this.initMap(location);
+    }
 
     this.currentPosMarker = document.querySelector('google-map .currentPos');
     this.currentPosMarker.latitude = this.pos.lat;
     this.currentPosMarker.longitude = this.pos.lng;
   },
+  initMap: function(location) {
+
+    this.map = document.querySelector('google-map');
+    this.map.zoom = 15;
+    this.map.latitude = this.pos.lat;
+    this.map.longitude = this.pos.lng;
+    this.loadCurrentPos();
+  },
   publish: function(e){
-    var index;
-    console.log('e.target.alt', e.target.alt);
     if (e.target.alt === 'Publish'){
       this.name = this.querySelector('#mark_name').value;
       this.fire('publish', {lat: this.pos.lat,
@@ -43,14 +49,18 @@ Polymer({
         name: this.name,
         confim: 0,
         complaint: 0});
-    }else if (e.target.id.startsWith('confirmButton')){
-      console.log('CONFIM');
-      index = e.target.id.split('-')[1];
-      this.fire('confirm', { mark: this.marks[index] });
-    }else if (e.target.id.startsWith('complaintButton')){
-      console.log('complaint');
-      index = e.target.id.split('-')[1];
-      this.fire('complaint', { mark: this.marks[index] });
+    }else if (e.target.alt === 'confirm'){
+      this.fire('confirm', { mark: this.marks[ this.getIndex(e.target) ] });
+    }else if (e.target.alt === 'complaint'){
+      this.fire('complaint', { mark: this.marks[ this.getIndex(e.target) ] });
     }
+  },
+  getIndex: function(target){
+    var parent = target.parentElement.parentElement;
+
+    var hiddenElement = parent.querySelector('input[type="hidden"]');
+    var label = parent.querySelector('.' + target.alt + 'Label');
+    label.textContent = parseInt(label.textContent) + 1;
+    return hiddenElement.value;
   }
 });
