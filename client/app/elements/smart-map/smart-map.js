8b3e55684c2ac3519f7bc2473dfddfac7f932249
Freddy Rodriguez <cristinamar2000@gmail.com>
2015-10-28 11:55:22 -0430

    mark routes

--- client/app/elements/smart-map/smart-map.js
+++ client/app/elements/smart-map/smart-map.js
@@ -1,14 +1,51 @@
+function getDistance(pos1, pos2){
+    var R = 6372.795477598;
+    var C = Math.PI/180
+    var lata = pos1.lat;
+    var lona = pos1.lng;;
+
+    var latb = pos2.lat;
+    var lonb = pos2.lng;
+
+    var distance = 2 * R * Math.asin(
+      Math.sqrt( Math.pow ( Math.sin(C * ( lata - latb ) / 2), 2)
+        + Math.cos( C * lata ) * Math.cos(C * latb)
+        * Math.pow (Math.sin(C * ( lonb - lona) / 2 ), 2))
+    );
+
+    return Math.floor(distance * 100) / 100;
+}
+
 Polymer({
   is: 'smart-map',
   listeners: {
-    'tap': 'publish'
+    'tap': 'tap'
   },
   properties:{
-    marks: Array
+    marks: Array,
+    pos: Object,
+    direction: Object
   },
+  observers: [
+    'changeMark(marks, pos)'
+  ],
   ready: function(){
     this.fire('map-ready');
     this.loadCurrentPos();
+  },
+  changeMark: function(marks, pos){
+
+    if (this.marks && this.pos){
+      var that = this;
+      this.marks.forEach((mark) => {
+        mark.distance = getDistance(this.pos, mark);
+
+        var distanceElement = that.$$('#' + mark.__firebaseKey__ + ' .distance');
+        distanceElement.textContent = mark.distance + ' KM';
+      });
+  
+      var sorted = this.marks.sort((a, b) => a.distance - b.distance);
+    }
   },
   loadCurrentPos: function(){
     if (navigator.geolocation) {
@@ -24,8 +61,8 @@ Polymer({
       lat: location.coords.latitude,
       lng: location.coords.longitude
     };
-
-    if (!this.map){
+    console.log('pos', this.pos);
+    if (!this.googleMap){
       this.initMap(location);
     }
 
@@ -35,13 +72,13 @@ Polymer({
   },
   initMap: function(location) {
 
-    this.map = document.querySelector('google-map');
-    this.map.zoom = 15;
-    this.map.latitude = this.pos.lat;
-    this.map.longitude = this.pos.lng;
+    this.googleMap = document.querySelector('google-map');
+    this.googleMap.zoom = 18;
+    this.googleMap.latitude = this.pos.lat;
+    this.googleMap.longitude = this.pos.lng;
     this.loadCurrentPos();
   },
-  publish: function(e){
+  tap: function(e){
     if (e.target.alt === 'Publish'){
       this.name = this.querySelector('#mark_name').value;
       this.fire('publish', {lat: this.pos.lat,
@@ -51,16 +88,30 @@ Polymer({
         complaint: 0});
     }else if (e.target.alt === 'confirm'){
       this.fire('confirm', { mark: this.marks[ this.getIndex(e.target) ] });
+      this.addOpinion(e.target);
     }else if (e.target.alt === 'complaint'){
       this.fire('complaint', { mark: this.marks[ this.getIndex(e.target) ] });
+      this.addOpinion(e.target);
+    }else if(e.target.alt === 'directions'){
+      this.markDirectionTo(this.marks[ this.getIndex(e.target) ]);
+    }else if (e.target.alt === 'search closer'){
+      this.markDirectionTo(this.marks[ 0 ]);
     }
+  },
+  markDirectionTo: function(mark){
+    var start = `${this.pos.lat}, ${this.pos.lng}`;
+    var end = `${mark.lat}, ${mark.lng}`;
+
+    this.direction = {start: start, end: end};
+  },
+  addOpinion: function(target){
+    var parent = target.parentElement.parentElement;
+    var label = parent.querySelector('.' + target.alt + 'Label');
+    label.textContent = parseInt(label.textContent) + 1;
   },
   getIndex: function(target){
     var parent = target.parentElement.parentElement;
-
     var hiddenElement = parent.querySelector('input[type="hidden"]');
-    var label = parent.querySelector('.' + target.alt + 'Label');
-    label.textContent = parseInt(label.textContent) + 1;
     return hiddenElement.value;
   }
 });
